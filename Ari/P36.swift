9dd35460dfa1c839378a22ae7a6a6e8189b0739f
Hiraku Ohno <hira@hira-pc.local>
2016-06-16 20:26:32 +0900

    P36

---
+++ Ari/P36.swift
@@ -0,0 +1,49 @@
+//
+//  P36.swift
+//  Ari
+//
+//  Created by Hiraku Ohno on 2016/06/16.
+//  Copyright © 2016年 Hiraku Ohno. All rights reserved.
+//
+
+class P36: Base {
+
+    var field: [[Character]]
+    let M: Int
+    let N: Int
+
+    init(input: [String]) {
+        self.field = input.map {Array($0.characters)}
+        self.M = input[0].characters.count
+        self.N = input.count
+    }
+
+    func main() {
+        var res = 0
+
+        for i in 0 ..< N {
+            for j in 0 ..< M {
+                if field[i][j] == "W" {
+                    dfs(i, y: j)
+                    res += 1
+                }
+            }
+        }
+
+        print(res)
+    }
+
+    func dfs(x: Int, y: Int) {
+        field[x][y] = "."
+
+        for dx in -1 ... 1 {
+            for dy in -1 ... 1 {
+                let nx = x + dx
+                let ny = y + dy
+                if 0 <= nx && nx < N && 0 <= ny && ny < M && field[nx][ny] == "W" {
+                    self.dfs(nx, y: ny)
+                }
+            }
+        }
+    }
+}
