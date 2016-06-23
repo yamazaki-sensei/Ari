50571d20511160c0f248e830701364d836672418
Hiraku Ohno <hiraku.ohno@glucose.jp>
2016-06-23 20:43:35 +0900

    P53

---
+++ Ari/P53.swift
@@ -0,0 +1,51 @@
+//
+//  P53.swift
+//  Ari
+//
+//  Created by Hiraku Ohno on 2016/06/23.
+//  Copyright © 2016年 Hiraku Ohno. All rights reserved.
+//
+
+class P53: Base {
+
+    typealias Prop = (w: Int, v: Int)
+
+    let n: Int
+    let items: [Prop]
+    let W: Int
+
+    var dp: [[Int]] = Array<Array<Int>>(count: 200, repeatedValue: Array<Int>(count: 200, repeatedValue: -1))
+
+    init(items: [Prop], W: Int) {
+        self.n = items.count
+        self.items = items
+        self.W = W
+    }
+
+    static func main() {
+        P52(items: [(2, 3), (1, 2), (3, 4), (2, 2)], W: 5).main()
+    }
+
+    func main() {
+        print(rec(0, j: W))
+    }
+
+    func rec(i: Int, j: Int) -> Int {
+        if 0 <= dp[i][j] {
+            return dp[i][j]
+        }
+
+        var res: Int
+
+        if i == n {
+            res = 0
+        } else if j < items[i].w {
+            res = rec(i + 1, j: j)
+        } else {
+            res = max(rec(i + 1, j: j), rec(i + 1, j: j - items[i].w) + items[i].v)
+        }
+
+        dp[i][j] = res
+        return res
+    }
+}
