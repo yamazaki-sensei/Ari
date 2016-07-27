82ef5087e4961ce8102979fbd69b242f130ceb14
Hiraku Ohno <hiraku.ohno@glucose.jp>
2016-07-27 22:08:24 +0900

    Implement binary search

--- Ari/ArrayExtension.swift
+++ Ari/ArrayExtension.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-
 enum ArrayExtensionError: ErrorType {
     case Generics
 }
@@ -17,15 +16,28 @@ protocol Target: Equatable, Comparable {
 }
 
 extension Int: Target { }
-
 extension Float: Target { }
-
 extension Double: Target { }
 
-extension Array {
+extension Array where Element: Target {
 
     func lowerBound<T: Target>(value: T) throws -> Int {
         guard value is Array.Generator.Element else { throw ArrayExtensionError.Generics }
         return 0
+    }
+
+    func binarySearch<T: Target>(value: T) throws -> Int {
+        guard value is Array.Generator.Element else { throw ArrayExtensionError.Generics }
+        var lo = 0
+        var hi = self.count
+        while lo < hi {
+            let mid = lo + (hi - lo) / 2
+            if (self[mid] as! T) < value {
+                lo = mid + 1
+            } else {
+                hi = mid
+            }
+        }
+        return lo
     }
 }
--- AriTest/ArrayExtensionTest.swift
+++ AriTest/ArrayExtensionTest.swift
@@ -12,6 +12,26 @@ class ArrayExtensionTest: XCTestCase {
 
     func testCallLowerBound() {
         let array: [Int] = [1, 2, 3, 4, 5]
-        let _ = try! array.lowerBound(3)
+        let a = try! array.lowerBound(3)
+        XCTAssert(a == 0)
+    }
+
+    func testBinarySearch() {
+        let array = [1, 2, 3, 4, 5]
+        XCTAssertTrue(try array.binarySearch(1) == 0)
+        XCTAssertTrue(try array.binarySearch(2) == 1)
+        XCTAssertTrue(try array.binarySearch(3) == 2)
+        XCTAssertTrue(try array.binarySearch(4) == 3)
+        XCTAssertTrue(try array.binarySearch(5) == 4)
+
+        var caught = false
+        do {
+            try array.binarySearch(0.1)
+        } catch ArrayExtensionError.Generics {
+            caught = true
+        } catch {
+            
+        }
+        XCTAssertTrue(caught)
     }
 }
